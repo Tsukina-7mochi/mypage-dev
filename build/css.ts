bb5da96625052458f8260ccdac8fa0cf33b11d73
Mooncake Sugar <Tsukina7mochi@gmail.com>
2025-06-18 12:51:32 +0900

    feat: abandon sass

--- build/css.ts
+++ build/css.ts
@@ -1,6 +1,5 @@
 import type * as esbuild from 'esbuild';
 import * as path from '@std/path';
-import { sassPlugin } from 'jsr:@tsukina-7mochi/esbuild-plugin-sass';
 
 export type BuildOptionsOptions = {
   srcPath: string;
@@ -12,11 +11,10 @@ export const buildOptions = (
   options: BuildOptionsOptions,
 ): esbuild.BuildOptions => ({
   entryPoints: [
-    path.join(options.srcPath, 'style.scss'),
+    path.join(options.srcPath, 'global.css'),
   ],
   outdir: options.destPath,
   bundle: true,
   sourcemap: options.dev ? 'inline' : 'linked',
   minify: !options.dev,
-  plugins: [sassPlugin()],
 });

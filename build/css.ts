5689a07f1faf53afacdc343309bcb12b938c321a
Mooncake Sugar <Tsukina7mochi@gmail.com>
2025-06-30 20:48:55 +0900

    feat: move assets directory out of src

--- build/css.ts
+++ build/css.ts
@@ -1,5 +1,6 @@
 import type * as esbuild from 'esbuild';
 import * as path from '@std/path';
+import { externalFontPlugin } from './plugins/externalFontPlugin.ts';
 
 export type BuildOptionsOptions = {
   srcPath: string;
@@ -11,10 +12,13 @@ export const buildOptions = (
   options: BuildOptionsOptions,
 ): esbuild.BuildOptions => ({
   entryPoints: [
-    { in: path.join(options.srcPath, 'global.css'), out: 'index' },
+    { in: path.join(options.srcPath, 'global.css'), out: 'style' },
   ],
   outdir: options.destPath,
   bundle: true,
   sourcemap: options.dev ? 'inline' : 'linked',
   minify: !options.dev,
+  plugins: [
+    externalFontPlugin,
+  ],
 });

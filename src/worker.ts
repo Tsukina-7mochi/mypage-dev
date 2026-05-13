6502db395b6bac50694c15f76a1be96fa9df1c4c
Mooncake Sugar <Tsukina7mochi@gmail.com>
2026-05-13 21:37:21 +0900

    fix: add error handling for worker

--- src/worker.ts
+++ src/worker.ts
@@ -4,21 +4,26 @@ import * as llyn from "llyn/runtime";
 
 export default {
   async fetch(req: Request): Promise<Response> {
-    const url = new URL(req.url);
-    const pathname = url.pathname;
+    try {
+      const url = new URL(req.url);
+      const pathname = url.pathname;
 
-    if (pathname.startsWith("/_islands/")) {
-      const id = pathname.slice(10);
-      const island = await llyn.renderIsland(id);
-      if (island === null) {
-        return new Response("not found", { status: 404 });
-      } else {
-        return new Response(island);
+      if (pathname.startsWith("/_islands/")) {
+        const id = pathname.slice(10);
+        const island = await llyn.renderIsland(id);
+        if (island === null) {
+          return new Response("not found", { status: 404 });
+        } else {
+          return new Response(island);
+        }
       }
-    }
 
-    return serveDir(req, {
-      fsRoot: path.join(import.meta.dirname ?? "", "static/"),
-    });
+      return serveDir(req, {
+        fsRoot: path.join(import.meta.dirname ?? "", "static/"),
+      });
+    } catch (err) {
+      console.error(err);
+      return new Response("internal server error", { status: 500 });
+    }
   },
 };

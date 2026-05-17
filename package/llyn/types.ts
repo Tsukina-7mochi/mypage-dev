589a8d6cdc31fe8c74d468517bbb2d4b8a76d0ef
Mooncake Sugar <Tsukina7mochi@gmail.com>
2026-05-17 16:37:25 +0900

    refactor(llyn): remove dead code

--- package/llyn/types.ts
+++ package/llyn/types.ts
@@ -1,9 +1,3 @@
-import { IdProvider } from "./idProvider.ts";
-import * as parse5 from "parse5";
-
-type Document = parse5.DefaultTreeAdapterTypes.Document;
-type DocumentFragment = parse5.DefaultTreeAdapterTypes.DocumentFragment;
-
 export type Island = {
   id: string;
   url: URL;
@@ -20,56 +14,3 @@ export type BuildOptions = {
   dist: string;
   markdownTemplate: string;
 };
-
-export type ProcessContext = {
-  options: {
-    root: URL;
-    dist: URL;
-    staticDist: URL;
-  };
-  idProvider: IdProvider;
-  registerSourceFile(input: string, output: string): void;
-  registerVirtualFile(input: string, output: string, content: string): void;
-  registerServerIsland(island: Island): void;
-};
-
-export type Processor<T extends string, I, O> = {
-  type: T;
-  process(input: I, ctx: ProcessContext): O;
-};
-
-export type HtmlFileProcessor = Processor<
-  "html",
-  URL,
-  Promise<void>
->;
-export type HtmlDocumentProcessor = Processor<
-  "html-document",
-  { url: URL; content: string },
-  Promise<{ document: Document }>
->;
-export type HtmlDocumentFragmentProcessor = Processor<
-  "html-document-fragment",
-  { url: URL; content: string },
-  Promise<{ documentFragment: DocumentFragment }>
->;
-export type MarkdownProcessor = Processor<
-  "markdown",
-  { file: URL; template: URL },
-  Promise<void>
->;
-export type ClientIslandProcessor = Processor<
-  "client-island",
-  Island,
-  Promise<{ prerender: string }>
->;
-export type ServerIslandProcessor = Processor<
-  "server-island",
-  Island,
-  Promise<{ prerender: string }>
->;
-export type BuildAssetProcessor = Processor<
-  "build-asset",
-  URL,
-  { outFile: URL }
->;
